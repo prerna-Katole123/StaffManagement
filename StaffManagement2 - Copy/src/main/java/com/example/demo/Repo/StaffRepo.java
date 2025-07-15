df279114c2e768b0d7e2a7e8132836a7bcdf95fa
YogitaTayde <yogitatayde9@gmail.com>
2025-07-14 23:14:59 -0700

    service and repo added

--- StaffManagement2 - Copy/src/main/java/com/example/demo/Repo/StaffRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.Repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.Entity.Staff;
-
-public interface StaffRepo extends JpaRepository<Staff, Integer> {
-
-}
